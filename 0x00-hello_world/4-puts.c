9907b11b654552c42543f385f3f5ac9d7a2e26f8
KumieA88 <anny.kumako@yahoo.com>
2023-02-16 12:17:39 -0800

    Hello_puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,12 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
- *
- * Description: A program that will use puts function
- *
- * Return: Always 0 (success)
-*/
-
-int main(void)
+ * Description: A program that will use puts funtion
+ * Return: 0(success)
+ */
+int main(0)
 {
-puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
-Return:0;
+	Return 0;
 }
