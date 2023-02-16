b71e061fb978533dc42e58e3225d99dafb38c5c5
KumieA88 <anny.kumako@yahoo.com>
2023-02-16 12:23:12 -0800

    Hello_puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Description: A program that will use puts funtion
+ * Description: A program that will use puts function
  * Return: 0(success)
  */
 int main(0)
