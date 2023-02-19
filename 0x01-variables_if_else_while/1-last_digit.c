b7f4335226d1d3423981daa24321c104a00a9ed2
KumieA88 <anny.kumako@yahoo.com>
2023-02-19 12:03:52 -0800

    Alphabets

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,8 +12,8 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	lt_dgt = (n % 10)
-	if (lt_dgt < 6 && lt_dgt == 0)
+	lt_dgt = (n % 10);
+	if (lt_dgt < 6)
 		printf("Last digit of %d is %d and is less than 6 and not 0\n",
 		n, lt_dgt);
 	else if (n > 5)
