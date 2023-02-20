d6d8f94b63826eec7323b3d4288a7918ee7056a1
KumieA88 <anny.kumako@yahoo.com>
2023-02-20 02:46:52 -0800

    When I was having that alphabet soup

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,25 +14,24 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	'lst_dgt' = (n % 10);
-	if ((n % 10) == 0)
+	lstdgt = (n % 10);
+	if (lstdgt == 0)
 	{
-		printf("Last digit of %d is %d and is %s", n, lst_dgt, "0\n");
-	return (0);
-
+		printf("Last digit of %d is %d and is %s", n, lstdgt, "0\n");
+		
+		return (0);
 	}
 
-	if ((n % 10) < 6)
+	if (lstdgt < 6)
 	{
 		printf("Last digit of %d is %d and id %s",
-		n, lst_dgt, "less than 6 and not 0\n");
+		n, lstdgt, "less than 6 and not 0\n");
 	}
 
-	if ((n % 10) > 5)
+	if (lstdgt > 5)
 	{
-		printf("Last digit of %d is %d and is %s", n, lst_dgt, "greater than 5\n");
+		printf("Last digit of %d is %d and is %s", n, lstdgt, "greater than 5\n");
 
 	}
 	
-	return (o);
 }
---
+++ 0x01-variables_if_else_while/4-print_alphabat.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main- print alphabet in lowercase
+ * except for letter e and q
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch != 'e' && ch != 'q')
+			putchar(ch);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
