b3733266df7b1b4127fdf574021332242c079192
KumieA88 <anny.kumako@yahoo.com>
2023-02-20 03:39:11 -0800

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - program that print all digit numbers
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 10; num++)
+		putchar((num % 10) + '0');
+
+	putchar("\n")
+
+	return (0);
+}
