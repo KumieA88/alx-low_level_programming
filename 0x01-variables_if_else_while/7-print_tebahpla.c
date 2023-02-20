23166c90f19e72ece410c2e9ce8d0c6d131a7e68
KumieA88 <anny.kumako@yahoo.com>
2023-02-20 04:01:53 -0800

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - print the alphabet in reverse
+ * Return: Always 0
+ */
+int main(void)
+{
+	int alp;
+
+	for (alp = 'z'; alp <= 'a'; alp--)
+		putchar(alp);
+
+	putchar('\n');
+
+	return (0);
+}
