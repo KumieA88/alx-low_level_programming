4c910bba9d9b324ca54040040e0aa5fd2507a306
KumieA88 <anny.kumako@yahoo.com>
2023-02-20 05:03:27 -0800

    Patience, Persistence

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - print alll single combinations of single digit numbers
+ * Return: Always 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 48; a < 58; a++)
+	{
+		putchar(a);
+		if (a != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
