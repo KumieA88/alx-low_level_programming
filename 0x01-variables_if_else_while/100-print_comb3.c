72031d05d0d8d14d982325a28e07aec7c89c3bef
KumieA88 <anny.kumako@yahoo.com>
2023-02-20 07:06:23 -0800

    Inventing is a combination of brains and materials fix

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -4,24 +4,24 @@
  * Return: Always 0
  */
 int main(void)
-{
+}
 	int f, q;
 
 	for (f = '0'; f < '9'; f++)
-	{
+	}
 	for (q = f + 1; q <= '9'; q++)
 	{
 	if (q != f)
 	{
 	putchar(f);
 	putchar(q);
-	if (f == '8' && q == '9')
-	conitnue;
+	if (f == '8' && q == '9)
+		continue;
 	putchar(',');
 	putchar(' ');
 	}
 	}
 	}
-	putchar('\n')
+	putchar('\n');
 	return (0);
 }
