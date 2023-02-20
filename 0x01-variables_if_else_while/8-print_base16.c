cdabd5686c6769d269b1b60b7a1fe2d8c22c5e9c
KumieA88 <anny.kumako@yahoo.com>
2023-02-20 04:27:01 -0800

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include<stdio>
+/**
+ * main - print numbers of base sixteen in lowercase
+ * Return: Always 0
+ */
+int main(void)
+{
+	int ch;
+	char num;
+
+	for (num = '0'; num <= '9'; num++)
+		putchar(num);
+
+	for (ch = 'a'; ch <= 'f'; ch++)
+		putchar(ch);
+
+	putchar('\n');
+
+	return (0);
+}
