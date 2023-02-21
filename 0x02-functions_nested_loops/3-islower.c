ac962d7120e0dff678d7dd620cd6faa6b24c7c4f
KumieA88 <anny.kumako@yahoo.com>
2023-02-21 11:50:40 -0800

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _islower - program checks for _islower letter
+ * @c: is checked if its lowercase
+ * Return: 1 for lowercase or 0 for anything else
+ */
+int _islower(int c)
+{
+
+	if (c >= 'a' && c <= 'z')
+	{
+	
+	return (1);
+	}
+	else
+	return (0);
+}
