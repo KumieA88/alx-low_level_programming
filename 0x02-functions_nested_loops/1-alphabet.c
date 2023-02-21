e0dfc82304c06fc4f6ad16333c91cf7e2ff50c61
KumieA88 <anny.kumako@yahoo.com>
2023-02-21 06:27:02 -0800

    1. I sometimes suffer from insomnia

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - programe that prints the alphabet
+ * Return: Always 0
+ */
+int main(void)
+{
+	int alp;
+
+	for (alp = 'a'; alp <= 'z'; alp++)
+	_putchar(alp);
+
+	_putchar('\n');
+
+	return (0);
+}
