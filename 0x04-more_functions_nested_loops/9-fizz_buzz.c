077bbb0deb18650320e9ce2c9d4e5a42e0e699ff
KumieA88 <anny.kumako@yahoo.com>
2023-02-23 18:27:24 -0800

    9_Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - a program that prints Fizz buzz test
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int start = 1, end = 100;
+
+	while (start <= 100)
+	{
+		if (start % 3 == 0)
+			printf("Fizz");
+		if (start % 5 == 0)
+			printf("Buzz");
+		if (start % 3 != 0 && start % 5 != 0)
+			printf("%d", start);
+
+		putchar(start == end ? '\n' : ' ');
+		start++;
+	}
+
+	return (0);
+}
