65ec5535731a721bcc38b408b1fd442cfd5a33b2
KumieA88 <anny.kumako@yahoo.com>
2023-03-08 10:14:09 -0800

    6. Inception. Is it possible

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -10,8 +10,9 @@ int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
-int find_sqrt(int n, int i);
+int find_sqrt(int num, int root);
 int is_prime_number(int n);
+int actual_prime(int n, int i);
 int _putchar(char c);
 
 #endif
